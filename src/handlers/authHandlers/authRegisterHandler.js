28f61e78206bdc8ead35e3585ec6b9049fe64f3e
hinata <atahin0607@gmail.com>
2023-11-16 03:24:31 +0900

    アイコン、ヘッダーの乱数拡張

--- src/handlers/authHandlers/authRegisterHandler.js
+++ src/handlers/authHandlers/authRegisterHandler.js
@@ -8,7 +8,7 @@ const User = require("../../models/users");
 exports.authRegisterHandler = async (req, res, next) => {
     try {
         const uuid = uuidv4();
-        const randomInt = Math.floor(Math.random() * 6) + 1;
+        const randomInt = Math.floor(Math.random() * 8) + 1;
         const password = req.body.password;
         req.body.password = CryptoJS.AES.encrypt(password, process.env.PASSWORD_SECRET_KEY);
         const newUser = await new User({
