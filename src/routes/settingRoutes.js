19743407f9411ebd30cd46f3cf6a1a3eb886bb5d
hinata <atahin0607@gmail.com>
2023-12-10 00:31:22 +0900

    テーマ変更対応

--- src/routes/settingRoutes.js
+++ src/routes/settingRoutes.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const settingUserIdApi = require("../api/settingApi/settingUserIdApi");
 const settingPhoneNumberApi =require("../api/settingApi/settingPhoneNumberApi");
+const settingThemeApi = require("../api/settingApi/settingThemeApi");
 
 
 // APIルーティング
 
 router.use("/userId", settingUserIdApi);
 router.use("/phoneNumber", settingPhoneNumberApi);
+router.use("/theme", settingThemeApi);
 
 
 module.exports = router;
