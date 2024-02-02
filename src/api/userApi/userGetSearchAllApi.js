2fe461bf2bcac931681be4aca90d8190b4b0c88f
hinata <atahin0607@gmail.com>
2024-02-02 18:53:02 +0900

    ユーザー検索API完全化

--- src/api/userApi/userGetSearchAllApi.js
+++ src/api/userApi/userGetSearchAllApi.js
@@ -1,9 +1,11 @@
 const { getAllSearchUser } = require("../../controllers/userControllers/getAllSearchUser");
+const { getParamsUser } = require("../../controllers/userControllers/getParamsUser");
 const router = require("express").Router();
 
 // 検索ワードに合致するユーザーをページネーション対応で取得するAPI (userIdとusername)
 
-router.get("/",
+router.get("/:_id",
+    getParamsUser,
     getAllSearchUser,
     (req, res) => {
         return res.status(200).json({users: req.users, num: req.num});
